fa051f34f0457c1e2ba18f7c0d23c8cd0cc61bde
simar kalsi <singhsimar280@gmail.com>
2023-02-11 13:14:05 +0530

    commi2

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 require("dotenv").config();
-const express= require("express");
+const express = require("express");
 
-const app=express();
-const PORT=process.env.PORT || 5000
+const app = express();
+const PORT = process.env.PORT || 5000
 const products_routes = require("./routes/products")
-const connectDB=require("./db/connect")
+const connectDB = require("./db/connect")
+const cors = require('cors');
+const product = require('./models/product')
+
+app.use(express.json());
 
 
+app.use("/", products_routes)
 
-app.use("/",products_routes)
+app.post("/addtodoCards", (req, res) => {
+    console.log(req.body);
+    const user = new product(req.body);
+    user.save().then(() =>{
+        res.status(201).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
 
-const start=async()=>{
-    try{
+    res.send("hello guys");
+})
+
+const start = async () => {
+    try {
         await connectDB(process.env.MONGODB_URL);
-        app.listen(PORT,()=>{
+        app.listen(PORT, () => {
             console.log(`${PORT} Yes I am connected`);
         })
-    }catch(error){
+    } catch (error) {
         console.log(error)
 
     }
